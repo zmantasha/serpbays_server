b5261dc42998506dd7f68e949b9a7aca654493c2
Mantasha Zubair <mantasha@wordscloud.in>
2025-05-06 13:19:31 -0700

    convert typescript to javascript

--- src/api/user-wallet/routes/user-wallet.js
+++ src/api/user-wallet/routes/user-wallet.js
@@ -6,37 +6,4 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = {
-  routes: [
-    // Standard CRUD routes
-    ...createCoreRouter('api::user-wallet.user-wallet').routes,
-    // Custom wallet endpoints
-    {
-      method: 'GET',
-      path: '/api/wallet',
-      handler: 'user-wallet.getWallet',
-      config: {
-        policies: [],
-        auth: true
-      }
-    },
-    {
-      method: 'POST',
-      path: '/api/wallet/transaction',
-      handler: 'user-wallet.createTransaction',
-      config: {
-        policies: [],
-        auth: true
-      }
-    },
-    {
-      method: 'GET',
-      path: '/api/wallet/transactions',
-      handler: 'user-wallet.getTransactions',
-      config: {
-        policies: [],
-        auth: true
-      }
-    }
-  ]
-};
+module.exports = createCoreRouter('api::user-wallet.user-wallet');
