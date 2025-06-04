08c02ea2d947d0297ac7056aa3df0372eeabe85c
Mantasha Zubair <mantasha@wordscloud.in>
2025-06-04 17:36:05 +0530

    fix cors error

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,13 +1,38 @@
 module.exports = [
   'strapi::errors',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: [
+        'Content-Type',
+        'Authorization',
+        'Origin',
+        'Accept',
+        'Access-Control-Allow-Origin',
+        'Access-Control-Allow-Methods',
+        'Access-Control-Allow-Headers'
+      ],
+      origin: [
+        'http://localhost:3000',
+        'http://localhost:1337',
+        'http://localhost:3800',
+        'http://staging.serpbays.com',
+        'https://staging.serpbays.com',
+        'https://cms.serpbays.com'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      credentials: true,
+      maxAge: 86400
+    }
+  },
   {
     name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
+          'connect-src': ["'self'", 'https:', 'http:', 'ws:', 'wss:'],
           'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
           'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
           upgradeInsecureRequests: null,
