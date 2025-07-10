637f661ff6d8970249d5207c3188ca6118f751bf
Mantasha Zubair <mantasha@wordscloud.in>
2025-07-10 12:27:02 +0530

    improvements to the messages dropdown

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: ['yourKeyA', 'yourKeyB'],
+    keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
