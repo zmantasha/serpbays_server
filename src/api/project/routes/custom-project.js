53a571f922fde9a09384a19e84c4192544b58f36
zmantasha <mantasha@wordscloud.in>
2025-12-11 13:14:40 +0530

    fix(api-routes): removed duplicate `/api` prefix from project endpoint

--- src/api/project/routes/custom-project.js
+++ src/api/project/routes/custom-project.js
@@ -2,18 +2,18 @@
 
 module.exports = {
   routes: [
+    // {
+    //   method: 'GET',
+    //   path: '/projects/my/all',
+    //   handler: 'api::project.project.getMyProjects',
+    //   config: {
+    //     policies: ['api::project.is-authenticated'],
+    //     middlewares: [],
+    //   }
+    // },
     {
       method: 'GET',
-      path: '/api/projects/my/all',
-      handler: 'api::project.project.getMyProjects',
-      config: {
-        policies: ['api::project.is-authenticated'],
-        middlewares: [],
-      }
-    },
-    {
-      method: 'GET',
-      path: '/api/projects/templates',
+      path: '/projects/templates',
       handler: 'api::project.project.getTemplates',
       config: {
         policies: ['api::project.is-authenticated'],
@@ -22,25 +22,25 @@ module.exports = {
     },
     {
       method: 'POST',
-      path: '/api/projects/from-template',
+      path: '/projects/from-template',
       handler: 'api::project.project.createFromTemplate',
       config: {
         policies: ['api::project.is-authenticated'],
         middlewares: [],
       }
     },
-    {
-      method: 'POST',
-      path: '/api/projects/:id/team',
-      handler: 'api::project.project.addTeamMembers',
-      config: {
-        policies: ['api::project.is-authenticated'],
-        middlewares: [],
-      }
-    },
+    // {
+    //   method: 'POST',
+    //   path: '/projects/:id/team',
+    //   handler: 'api::project.project.addTeamMembers',
+    //   config: {
+    //     policies: ['api::project.is-authenticated'],
+    //     middlewares: [],
+    //   }
+    // },
     {
       method: 'GET',
-      path: '/api/projects/:id/analytics',
+      path: '/projects/:id/analytics',
       handler: 'api::project.project.getAnalytics',
       config: {
         policies: ['api::project.is-authenticated'],
@@ -49,7 +49,7 @@ module.exports = {
     },
     {
       method: 'PUT',
-      path: '/api/projects/:id/metrics',
+      path: '/projects/:id/metrics',
       handler: 'api::project.project.updateMetrics',
       config: {
         policies: ['api::project.is-authenticated'],
@@ -58,7 +58,7 @@ module.exports = {
     },
     {
       method: 'PUT',
-      path: '/api/projects/:id/archive',
+      path: '/projects/:id/archive',
       handler: 'api::project.project.archiveProject',
       config: {
         policies: ['api::project.is-authenticated'],
@@ -67,7 +67,7 @@ module.exports = {
     },
     {
       method: 'PUT',
-      path: '/api/projects/:id/unarchive',
+      path: '/projects/:id/unarchive',
       handler: 'api::project.project.unarchiveProject',
       config: {
         policies: ['api::project.is-authenticated'],
