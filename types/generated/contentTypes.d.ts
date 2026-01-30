503d5590c589202b8570869a7efdeedde5c00780
Mantasha Zubair <mantasha@wordscloud.in>
2026-01-30 13:25:13 +0530

    fix: keep Rank validation at minimum 1 for metrics update

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -2015,7 +2015,7 @@ export interface ApiPublisherWebsitePublisherWebsite
       Schema.Attribute.DefaultTo<0>;
     description: Schema.Attribute.Text &
       Schema.Attribute.SetMinMaxLength<{
-        maxLength: 200;
+        maxLength: 1000;
       }>;
     detailsCompletedAt: Schema.Attribute.DateTime;
     doCopywriting: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
@@ -2051,7 +2051,7 @@ export interface ApiPublisherWebsitePublisherWebsite
     gscVerifiedAt: Schema.Attribute.DateTime;
     guidelines: Schema.Attribute.Text &
       Schema.Attribute.SetMinMaxLength<{
-        maxLength: 600;
+        maxLength: 5000;
       }>;
     isPRSite: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
     language: Schema.Attribute.JSON & Schema.Attribute.DefaultTo<['English']>;
