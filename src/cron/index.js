1944e0b3995d452e1ebc21c011bbac7a8f5cb6a9
zmantasha <mantasha@wordscloud.in>
2026-03-02 20:43:05 +0530

    feat: Autosend marketing email automation
    - Abandoned cart list: add/remove users when cart items change
    - Wallet funded, no order list: add after wallet top-up if zero orders
    - Inactive after signup cron: daily at midnight, targets users 3 days old with no activity
    - Win-back churned buyers cron: daily at 1 AM, targets users whose last order was 30 days ago
    - Biweekly newsletter cron: sends top 5 new high-DA websites to all confirmed users
    - Order creation removes users from all marketing lists (abandoned cart, wallet, inactive signup, win-back)
    - Added sendNewsletter() method with batching and unsubscribe group support
    - Added newsletterEnabled toggle to Global Config (converted to singleType)
    - Added addToList() and removeFromList() methods to autosend-service

--- src/cron/index.js
+++ src/cron/index.js
@@ -2,8 +2,10 @@
 
 const tatUpdater = require('./tat-updater');
 const orderCancellation = require('./order-cancellation');
+const autosendMarketing = require('./autosend-marketing');
 
 module.exports = {
     ...tatUpdater,
     ...orderCancellation,
+    ...autosendMarketing,
 };
